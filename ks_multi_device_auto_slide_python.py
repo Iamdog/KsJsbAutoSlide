d2e7e6b08652ce6e64988d93622a3795ee1269e4
liuqiangwu <15972730759@163.com>
2020-11-13 10:37:46 +0800

    支持多个Android设备同时操作

---
+++ ks_multi_device_auto_slide_python.py
@@ -0,0 +1,59 @@
+import os,subprocess
+import re
+from datetime import datetime
+import time
+import random
+
+
+# 随机数的范围
+startRange = 10
+endRange = 20
+# 秒数 
+TIME = 15
+# 100 1000为滑动开始坐标
+# 100 200为滑动停止坐标
+# 300 为滑动时长
+SHELLCOMMAND = "./adb -s  {} shell  input swipe 100 1000 100 200 300"
+# 启动快手极速版
+STARTKSJSBAPP = "./adb -s {} shell am start -W -n com.kuaishou.nebula/com.yxcorp.gifshow.HomeActivity"
+
+def adbShellCommand(device):
+    os.system(STARTKSJSBAPP.format(device))
+    os.system(SHELLCOMMAND.format(device))
+
+
+# 获取多个adb连接设备
+def getAllAdbDevices():
+    deviceInfo= os.popen("./adb devices").read()
+    deviceInfo  = deviceInfo.replace("List of devices attached","").replace('\n', '').replace('\r', '').replace('\t','')
+    deviceNames = deviceInfo.split("device")
+    return deviceNames
+
+# 每n秒执行一次
+def timer():
+    while True:
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        deviceNames = getAllAdbDevices()
+
+        if len(deviceNames)==0:
+            continue
+        
+        for device in deviceNames:
+            if device == '':
+                continue
+            adbShellCommand(device)
+        
+        randomNum = random.randint(startRange,endRange)
+        print("时长:>>>>",randomNum)
+        time.sleep(randomNum)
+
+
+
+
+
+
+def main():
+    timer()
+
+if __name__ == '__main__':
+    main()
